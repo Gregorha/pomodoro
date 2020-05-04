e8a00c183538c60a88f334355b2c4e511a401b09
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-05-04 11:36:52 -0300

    Updates

---
+++ precache-manifest.3585da391bafd2c9d09fb6845b7e4bee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb98f43c1b20a72b9f464de57762cb30",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "bbaf8bb3a91b635f22c8",
+    "url": "/pomodoro/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "97d2fb8522fef0c2b942",
+    "url": "/pomodoro/static/css/main.3ce92e75.chunk.css"
+  },
+  {
+    "revision": "bbaf8bb3a91b635f22c8",
+    "url": "/pomodoro/static/js/2.90dd4cb9.chunk.js"
+  },
+  {
+    "revision": "0c710714cc0812ad904dc577eda8f829",
+    "url": "/pomodoro/static/js/2.90dd4cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97d2fb8522fef0c2b942",
+    "url": "/pomodoro/static/js/main.6696f003.chunk.js"
+  },
+  {
+    "revision": "42337fb65940131f50b9",
+    "url": "/pomodoro/static/js/runtime-main.a098e6ba.js"
+  },
+  {
+    "revision": "2d55e753b889c152357d3f8ea6151b62",
+    "url": "/pomodoro/static/media/inflicted.2d55e753.mp3"
+  }
+]);
