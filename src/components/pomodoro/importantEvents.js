b242d475b6cca127750000035ff513c9086090a1
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-05-06 13:19:35 -0300

    update important event time

--- src/components/pomodoro/importantEvents.js
+++ src/components/pomodoro/importantEvents.js
@@ -1,7 +1,7 @@
 const { DateTime } = require("luxon");
 const importantEvents = [
   {
-    date: DateTime.local().set({hours: 13, minutes: 18 , seconds: 0 , milliseconds: 0}), label: "Aula"
+    date: DateTime.local().set({hours: 16, minutes: 30 , seconds: 0 , milliseconds: 0}), label: "Aula"
   },
   {
     date: DateTime.local().set({hours: 19, minutes: 20 , seconds: 0 , milliseconds: 0}), label: "Forms"
