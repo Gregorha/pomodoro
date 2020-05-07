992f78617e6939d2bc32c4dfac0cff8180d626fc
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-05-06 21:16:38 -0300

    updated import event hour

--- src/components/pomodoro/importantEvents.js
+++ src/components/pomodoro/importantEvents.js
@@ -4,7 +4,7 @@ const importantEvents = [
     date: DateTime.local().set({hours: 16, minutes: 30 , seconds: 0 , milliseconds: 0}), label: "Aula"
   },
   {
-    date: DateTime.local().set({hours: 19, minutes: 20 , seconds: 0 , milliseconds: 0}), label: "Forms"
+    date: DateTime.local().set({hours: 19, minutes: 30 , seconds: 0 , milliseconds: 0}), label: "Forms"
   },
   {
     date: DateTime.local().set({hours: 19, minutes: 40 , seconds: 0 , milliseconds: 0}), label: "Fechamento"
